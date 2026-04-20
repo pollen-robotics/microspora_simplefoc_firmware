6c3846ea60aabede2ac5d8c02c4063209d3b2075
gospar <antun.skuric@outlook.com>
2026-04-20 11:36:37 +0200

    a new pass for the new baord

--- README.md
+++ README.md
@@ -1,29 +1,111 @@
 Microspora board firmware based on SimpleFOC using platformIO
 ========================================
 
+<img src="docs/top.png" height="300"><img src="docs/bck.png" height="300">
+
 A template platformIO project for the microspora firmware using SimpleFOC.
 
-- microspora by [@Rambros](https://github.com/rambros3d) : https://oshwlab.com/rambros/nano-8316-motor-driver
-- SimpleFOC by [@simplefoc](https://github.com/simplefoc/Arduino-FOC): https://simplefoc.com/
+> microspora has been originally developed by [@Rambros](https://github.com/rambros3d) - See [Github](https://github.com/rambros3d/MicroSpora-SimpleFOC) 
+
 
 This is the firmware for the microspora board, a compact motor driver board based on the DRV8316 IC and an STM32G4 series microcontroller, and MT6701 encoder. The board is designed for BLDC motors under 10Amps, and is compatible with the SimpleFOC library. It is super compact and it seems to be a very very nice fit to start with SimpleFOC!
 
-It can be used for torque, velocity and position control.
 
 ## Requirements
 
-You'll need to have [PlatformIO](https://platformio.org/) installed and to download the SimpleFOC into your `lib` folder. 
+You'll need to have [PlatformIO](https://platformio.org/) and download the code from this repository. 
 
-I'd suggest you to do it like this:
-
-```
-cd your_project_folder/lib
-git clone https://github.com/simplefoc/Arduino-FOC.git
-cd Arduino-FOC
-git checkout dev
+```bash
+git clone git@github.com:simplefoc/microspora_simplefoc_firmware.git
 ```
 
-> You'll need the `dev` branch until the next release `2.3.6`. Once it is out, you can just add `askuric/Arduino FOC` to your `lib_deps` in `platformio.ini` and skip this step.
+## Code features
+- Ful SimpleFOC firmware
+    - Real-time control loop using hardware timer interrupts (10kHz)
+    - Driver and sensor fully configured for the microspora board
+      - DRV8316 config
+         - 6PWM
+         - High slew rate (200V/us)
+         - 5V buck output
+         - 300mV/A current sensing gain
+      - MT6701 config
+         - 10MHz SPI communication
+         - Setup for eccentricity compensation (optional - commented by default)
+    - Motor characterization and current controller tuning functions
+    - Use communication 
+        - Commander interface configured for webcontoller: https://webcontroller.simplefoc.com/
+        - CAN communication using SimpleFOC CAN protocol (optional - it does nothing if no CAN messages are sent/received)
+            - Example Python scripts for testing CAN communication with the microspora using the SimpleFOC CAN protocol
 
-And you're done, you can start developing your project!
+
+## Board features
+- Compact design 33mm x 32mm
+- Fully SimpleFOC compatible firmware
+    - allows for BLDCs and Steppers (hybrid stepper mode)
+- DRV8316 motor driver IC
+    - 8A peak current
+    - Integrated buck converter for 5V output
+    - Integrated current sensing (configurable gain)
+- STM32G4 series microcontroller (STM32G431CBU6)
+- MT6701 magnetic encoder 
+    - 14-bit resolution
+    - 10MHz SPI communication
+- CAN communication 
+    - Daisy chainable JST SH connectors for CAN bus
+- USBC connector
+    - Serial commnunication
+    - DFU firmware programming
+- On-board LED for status indication
+- Additional connectors
+    - 6pin JST - SPI communication (SPI3 the same as DRV)
+    - 5pin JST 
+        - I2C communication (SCL/SDA)  or
+          - can be used for STEMMA or QWIIC
+        - UART communication (TX/RX) or
+        - Encoder interface (A/B/I) or
+        - PWM input/output  or
+        - Step/dir input or
+        - 3 GPIOs 
+    
+
+
+
+## Programming and usage
+
+<img src="docs/dfu_btns.png" height="300">
+
+To program the board you will need to put it in DFU mode by pressing the BOOT while powering it on. The simplest way is to connect the board to your computer using the USBC connector, and then :
+1. press and hold the BOOT button 
+2. pressing the RESET button, 
+4. then release the RESET button while still holding the BOOT button 
+5. After a second or two release the BOOT button.
+
+The board should now be in DFU mode and you can upload the firmware using platformIO. 
+The same procedure can be used if the board is powered by an external power supply, but you will need to connect the board to your computer using the USBC connector for the programming.
+
+## Pinout 
+
+### 5pin JST connector (I2C/UART/Encoder/PWM/Stepdir)
+
+| Pin | Name| GPIO| I2C | UART | Encoder | PWM | Step/Dir | 
+| --- | --- | --- | --- | --- | --- | --- | --- | 
+| 1 | GND (bottom) |  | |
+| 5 | 3.3V |  | | | 
+| 2 | PA14 | IO | SDA (I2C1) | RX (UART2) | CH2 (TIM8) | CH2 (TIM8) or | Direction or
+| 3 | PA15 | IO | SCL (I2C1) |  TX (UART2) | CH1 (TIM8) | CH1 (TIM8) | Direction
+| 4 | PB6 | IO | -  |  | ETR (TIM8) | Direction | STEP (TIM8) |
+
+> Even though both QWIIC and Stemma have 4pin jst connectors they can be connected to the 5pin JST connector of the microspora, leaving the 5th pin PB6 unconnected. The pinout of the first 4 pins is the same. 
+
+
+### 6pin JST connector (SPI)
+
+| Pin | Name| SPI |
+| --- | --- |  --- |
+| 1 | GND (bottom) |  |
+| 6 | 3.3V | |
+| 2 | PB5 |  MOSI (SPI3) |
+| 3 | PB4 | MISO (SPI3) |
+| 4 | PC10 |  SCK (SPI3) |
+| 5 | PC11 | CS (SPI3) |
 
--- src/main.cpp
+++ src/main.cpp
@@ -4,36 +4,48 @@
 
 #include "Arduino.h"
 #include <SimpleFOC.h>
+#include "current_sense/hardware_specific/stm32/stm32g4/stm32g4_hal.h"
 #include <SimpleFOCDrivers.h>
 #include "drivers/drv8316/drv8316.h"
 #include "encoders/mt6701/MagneticSensorMT6701SSI.h"
+#include "encoders/calibrated/CalibratedSensor.h"
+#include "comms/can/CANCommander.h"
+#include "SimpleCANio.h"
+
+#define MOT_POLE_PAIRS 14
 
 // motor instance
-BLDCMotor motor = BLDCMotor(11);
+BLDCMotor motor = BLDCMotor(MOT_POLE_PAIRS); // 7 pole pairs, 2.04 Ohm, 0.35 mH
 DRV8316Driver6PWM driver = DRV8316Driver6PWM(PHA_H, PHA_L, PHB_H, PHB_L, PHC_H, PHC_L, DRV_CS, false);
 
 // sensor instance
-MagneticSensorMT6701SSI sensor = MagneticSensorMT6701SSI(ENC_CS);
+SPISettings MT6701SSISettings_new(10000000, MT6701_BITORDER, SPI_MODE2);
+MagneticSensorMT6701SSI sensor = MagneticSensorMT6701SSI(ENC_CS, MT6701SSISettings_new);
 
 // current sense instance
 LowsideCurrentSense current_sense = LowsideCurrentSense(375.0f, (int)ISENS_A, (int)ISENS_B, (int)ISENS_C);
 
+// instantiate the calibrated sensor object, 
+// CalibratedSensor sensor_calibrated = CalibratedSensor(sensor,, 200); // 200 points in the LUT
 
 // SPI configuration for the encoder and driver
 SPIClass SPI_1(ENC_NC, ENC_SDO, ENC_CLK); // use the SPI1 for the encoder
-SPIClass SPI_3(PB5_ALT1, PB4_ALT1, PB3_ALT1); // use SPI3 for the driver ( need to use alternate pins for SCK, MOSI and MISO)
+SPIClass SPI_3(PB5_ALT1, PB4_ALT1, PC10); // use SPI3 for the driver ( need to use alternate pins for SCK, MOSI and MISO)
+
+
+CANio can(CAN_RX, CAN_TX, NC, CAN_ENABLE); // Create CAN object
+CANCommander command_can(can, 15); // listen at the address 15 on the CAN bus
 
 // instantiate the commander
 Commander command = Commander(Serial);
-
-// simple function to be called upon recieving commands
+// simple function to be called upon receiving commands
 void doMotor(char* cmd){
   command.motor(&motor, cmd);
 }
 
 
 void setup() {
-  _delay(3000);
+  _delay(6000);
   // use monitoring with serial
   Serial.begin(250000);
   // enable more verbose output for debugging
@@ -45,8 +57,8 @@ void setup() {
 
   Serial.println("Initializing...");
 
-  //motor.sensor_direction = Direction::CCW;
-  //motor.zero_electric_angle = 3.6938;
+  // motor.sensor_direction = Direction::CCW;
+  // motor.zero_electric_angle = 3.00;
   digitalWrite(ENC_CS, HIGH);
 
   sensor.init(&SPI_1);        //  Initialize sensor on SPI_1 bus
@@ -54,66 +66,93 @@ void setup() {
 
   Serial.print("Sensor initialized: ");
 
-  // driver config
-  driver.voltage_power_supply = 20.0;
-  driver.pwm_frequency = 25000;
-  driver.dead_zone = 0.001f;
-  driver.init(&SPI_3);
-  driver.setSlew(Slew_200Vus);
-  driver.setCurrentSenseGain(Gain_0V375);
-  driver.setOCPMode(NoAction);
-  driver.setPWMMode(PWM6_Mode);
-  driver.setBuckVoltage(VB_5V);
+  float voltage =_readRegularADCVoltage(VSENS)* VSCALE; 
+  Serial.print("Supply voltage: ");
+  Serial.print(voltage);
+  Serial.println(" V");
 
+  while(voltage < 6.0f){
+    Serial.println("Voltage too low for motor operation!");
+    _delay(1000);
+  }
+
+
+  // driver config
+  driver.voltage_power_supply = voltage;
+  driver.pwm_frequency = 25000;
+  driver.dead_zone = 0.001f; 
+  driver.init(&SPI_3);
+  driver.setSlew(Slew_200Vus); // add the highest slew rate to make the voltage changes sharper
+  driver.setCurrentSenseGain(Gain_0V375); // configure the most sensitive gain for the current sense measurements
+  driver.setOCPMode(NoAction); 
+  driver.setPWMMode(PWM6_Mode); 
+  driver.setBuckVoltage(VB_5V);
 
   Serial.println("Driver initialized");
 
   current_sense.linkDriver(&driver);
   current_sense.init();
+  current_sense.skip_align = true; // no need to align 
   motor.linkCurrentSense(&current_sense);
   Serial.println("Current sense initialized");
 
-  motor.linkDriver(&driver);                          // link the motor and the driver
-  motor.foc_modulation = FOCModulationType::SinePWM;  // set FOC modulation type
+  motor.linkDriver(&driver);                          // link the motor and the dr
   // TorqueControlType::foc_current
-  motor.torque_controller = TorqueControlType::voltage;
+  motor.torque_controller = TorqueControlType::foc_current;
   // set motion control loop to be used
   motor.controller = MotionControlType::torque;
-
   motor.useMonitoring(Serial);  // comment out if not needed
-
+  motor.monitor_end_char = 'M'; // set monitoring end character to M for better parsing in the odrive tool
+  motor.monitor_start_char = 'M'; // set monitoring start character to S for better parsing in the odrive tool
   // init motor hardware
   if (!motor.init()) {
     Serial.println("Motor init failed!");
     return;
   }
 
+  // sensor eccentricity calibration if necessary
+  // sensor_calibrated.voltage_calibration = 2.0f; // voltage to be applied during the calibration
+  // sensor_calibrated.calibrate(motor); // run the calibration
+  // motor.linkSensor(&sensor_calibrated);  // link the motor to the sensor
+  // sensor_calibrated.printLUT(motor, Serial); // print the LUT to serial monitor
+  // sensor_calibrated.min_elapsed_time = 0.0005f; // set minimum elapsed time between updates to 500us
   motor.initFOC();
   Serial.println("Init complete...");
+  
+  command_can.init();
+  command_can.addMotor(&motor);
 
   // subscribe motor to the commander
   command.add('M', doMotor, "Set motor target");
 
+  // determine the motor parameters (optional)
+  motor.characteriseMotor(3.0f); // characterise motor with 2V voltage
+  motor.tuneCurrentController(100.0f); // tune current controller with 100rad/s bandwidth
+  
   digitalWrite(LED_BUILTIN, LOW);
   _delay(200);
+
+  // create a hardware timer
+  // For example, we will create a timer that runs at 10kHz on the TIM8
+  HardwareTimer* timer = new HardwareTimer(TIM8);
+  // Set timer frequency to 10kHz
+  timer->setOverflow(10000, HERTZ_FORMAT); 
+  // add the loopFOC and move to the timer
+  timer->attachInterrupt([](){
+    // call the loopFOC and move functions
+    motor.loopFOC();
+    motor.move();
+  });
+  // start the timer
+  timer->resume();
 }
-
-
+// do non-time critical tasks in the main loop
 void loop() {
-
-  // main FOC algorithm function
-  motor.loopFOC();
-
-  // Motion control function
-  // velocity, position or torque (defined in motor.controller)
-  // this function can be run at much lower frequency than loopFOC() function
-  // You can also use motor.move() and set the motor.target in the code
-  motor.move();
-
-  // function intended to be used with serial plotter to monitor motor variables
-  // significantly slowing the execution down!!!!
-  // motor.monitor();
+  // monitor the motor variables and send them to serial
+  motor.monitor();
 
   // user communication
   command.run();
+  // CAN communication (does nothing if no can sent/received)
+  command_can.run();
 }
